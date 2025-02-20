b117aea348c59ce1e2b046748b70cfd439edb803
abreu369 <81336647+2000zua@users.noreply.github.com>
2025-02-20 14:48:11 +0300

    last update

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("mysql+pymysql://zua:password@host:port/todoAPP")
+engine = create_engine("mysql+pymysql://zua:password@0.0.0.0:3306/todoAPP")
 Session = sessionmaker(bind=engine)
 
 def get_db():
