f9d7220f7e68287d7eb3f722b8fa54d5d3f2f452
abreu369 <81336647+2000zua@users.noreply.github.com>
2025-02-20 14:59:30 +0300

    last updated

--- database.py
+++ database.py
@@ -1,12 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine("mysql+pymysql://zua:password@0.0.0.0:3306/todoAPP")
-Session = sessionmaker(bind=engine)
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://zua:@127.0.0.1:3306/todoAPP"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
 
 def get_db():
     db = Session()
     try:
         yield db
+    except:
+        print("Something went wrong")
     finally:
         db.close()
+
+
