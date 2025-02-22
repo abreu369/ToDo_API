3f423045612c6f38b270c688fde00adefa33270c
abreu369 <81336647+2000zua@users.noreply.github.com>
2025-02-22 13:58:37 +0300

    last updated

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://zua:@127.0.0.1:3306/todoAPP"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@127.0.0.1:3306/todoAPP"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
